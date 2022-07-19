3f013658380605d35f30205cc1a83708da193001
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-19 14:14:53 -0300

    feat: recuperando bean do container spring para testar a configuração profile

--- src/main/java/com/ppfurtado/sfgdi/SfgDiApplication.java
+++ src/main/java/com/ppfurtado/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package com.ppfurtado.sfgdi;
 
-import com.ppfurtado.sfgdi.controller.ConstructorInjeectedController;
-import com.ppfurtado.sfgdi.controller.MyController;
-import com.ppfurtado.sfgdi.controller.PropertyInjectedController;
-import com.ppfurtado.sfgdi.controller.SetterInjectedController;
+import com.ppfurtado.sfgdi.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,10 @@ public class SfgDiApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+        System.out.println("--------- My Bean Profile");
+        I18NController i18NController = context.getBean("i18NController", I18NController.class);
+        System.out.println(i18NController.sayHello());
 
         System.out.println("--------- My Bean Primary");
         MyController theBean = context.getBean( "myController", MyController.class);
