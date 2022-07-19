b8043dce9ad5e831454e6adc144bca43a479e393
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-19 06:48:13 -0300

    refactor: usando o context para recuperar o bean do container spring

--- src/main/java/com/ppfurtado/sfgdi/SfgDiApplication.java
+++ src/main/java/com/ppfurtado/sfgdi/SfgDiApplication.java
@@ -1,13 +1,22 @@
 package com.ppfurtado.sfgdi;
 
+import com.ppfurtado.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+        MyController theBean = context.getBean( "myController", MyController.class);
+
+        String result = theBean.sayHello();
+
+        System.out.println(result);
+
     }
 
 }
