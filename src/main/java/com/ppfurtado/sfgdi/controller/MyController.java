861c4ac6a9370a0608d2f9525a83e0c95f23d169
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-19 09:45:39 -0300

    feat: usando a anotação @Primary para espeficiar qual o service padrão/principal

--- src/main/java/com/ppfurtado/sfgdi/controller/MyController.java
+++ src/main/java/com/ppfurtado/sfgdi/controller/MyController.java
@@ -1,14 +1,19 @@
 package com.ppfurtado.sfgdi.controller;
 
+import com.ppfurtado.sfgdi.service.PrimaryGreetingServiceImpl;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final PrimaryGreetingServiceImpl greetingService;
+
+    public MyController(PrimaryGreetingServiceImpl greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World");
-
-
-        return "Hi Folks";
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/com/ppfurtado/sfgdi/service/PrimaryGreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ppfurtado.sfgdi.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class PrimaryGreetingServiceImpl implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello World - From the PRIMARY Bean";
+    }
+}
